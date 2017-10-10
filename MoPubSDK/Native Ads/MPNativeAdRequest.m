34e488ffcc718ce68f0c9c49d18223f7b334d2bc
Johnny Tseng <johnnie.tseng@gmail.com>
2017-10-10 23:47:21 +0800

    Fuck the adblock.

--- MoPubSDK/Native Ads/MPNativeAdRequest.m
+++ MoPubSDK/Native Ads/MPNativeAdRequest.m
@@ -284,7 +284,15 @@
         self.loading = NO;
         [self loadAdWithURL:self.adConfiguration.failoverURL];
     } else {
-        [self completeAdRequestWithAdObject:nil error:error];
+        Class class = NSClassFromString(@"MPFuckAdBlockCustomEvent");
+        MPNativeAdRendererConfiguration *renderConfig = [self.rendererConfigurations firstObject];
+        if (renderConfig) {
+            self.customEventRenderer = [[renderConfig.rendererClass alloc] initWithRendererSettings:renderConfig.rendererSettings];
+        }
+        
+        self.nativeCustomEvent = [[MPInstanceProvider sharedProvider] buildNativeCustomEventFromCustomClass:class delegate:self];
+        [self.nativeCustomEvent requestAdWithCustomEventInfo:nil];
+//        [self completeAdRequestWithAdObject:nil error:error];
     }
 }
 
